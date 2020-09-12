a64b8c634e431dd8ffdf3c8cf74bfb41c8f958a0
Karla Agraz <karla.agraz@gmail.com>
2020-09-11 20:36:11 -0500

    counter fixed

--- js/animated-counter.js
+++ js/animated-counter.js
@@ -1,22 +1,15 @@
 const counters = document.querySelectorAll('.counter');
-const speed = 200; // The lower the slower
+const speed = 2000;
 
 counters.forEach(counter => {
 	const updateCount = () => {
 		const target = +counter.getAttribute('data-target');
 		const count = +counter.innerText;
 
-		// Lower inc to slow and higher to slow
 		const inc = target / speed;
 
-		// console.log(inc);
-		// console.log(count);
-
-		// Check if target is reached
 		if (count < target) {
-			// Add inc to count and output in counter
-			counter.innerText = count + inc;
-			// Call function every ms
+			counter.innerText = Math.ceil(count + inc);
 			setTimeout(updateCount, 1);
 		} else {
 			counter.innerText = target;
